a4565b0f0536425ca119edb8dd3ab674dd4c6fb6
jo_c <93196517+Jo-c25@users.noreply.github.com>
2022-11-02 01:30:22 +0100

    added a function that returns the winner of a single game

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function getComputerChoice(){
     let randomNum = Math.floor( Math.random() * 3) 
     return randomChoices[randomNum]
 }
+
+function playRound(playerSelection, computerSelection){
+if(playerSelection == computerSelection){
+    return "it is a tie"
+}else if(playerSelection == "Rock" && computerSelection == "Scissor" || playerSelection == "Scissor" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"){
+    return `You Win! ${playerSelection} beats ${computerSelection}`
+}else if(playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Scissor" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Scissor"){
+    return `You Lose! ${computerSelection} beats ${playerSelection}`
+}
+}
